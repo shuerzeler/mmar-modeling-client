4bb3646c2f75279d23edf5c5f815378dcd90358a
shuerzeler <114054468+shuerzeler@users.noreply.github.com>
2026-07-09 09:49:45 +0000

    add check for column mapping is selected by user before calling pm-service

--- src/dialogs/dialog-process-mining/dialog-process-mining.ts
+++ src/dialogs/dialog-process-mining/dialog-process-mining.ts
@@ -73,11 +73,17 @@ export class DialogProcessMining {
             const formData = new FormData();
             formData.append("file", this.selectedFile);
  
-            // Append CSV column mapping
+            // check is column mapping is filled
             if (this.isCsv) {
-                formData.append("case_id", this.caseId || "case:concept:name");
-                formData.append("activity_name", this.activityName || "concept:name");
-                formData.append("timestamp", this.timestamp || "time:timestamp");
+                if (!this.caseId || !this.activityName || !this.timestamp) {
+                    this.statusMessage = "Please map all CSV columns before running.";
+                    this.statusColor = "red";
+                return;
+                }
+                // Append CSV column mapping
+                formData.append("case_id", this.caseId);
+                formData.append("activity_name", this.activityName);
+                formData.append("timestamp", this.timestamp);
             }
  
             const response = await fetch(
